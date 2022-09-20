20ef21f85544de91e7882ceb03be939acba6c509
Fred Snyder <safe.sand1626@fastmail.com>
2022-09-20 16:11:41 +0000

    Add styling for the overlay menu

--- src/components/navMenu.tsx
+++ src/components/navMenu.tsx
@@ -8,20 +8,38 @@ export function NavMenu(props) {
 	const currentYear = new Date().getFullYear();
 
 	return (
-		<div id="menu">
-			<img src={iconLogo.toString()} alt="Shebang" />
-			<img src={iconCross.toString()} alt="Cross" />
-			<img src={imgShebang.toString()} alt="The Shebang in 3D" />
-			App info
-			{props.visible}
-			<div>
-				<p>
-					Copyright &#169; {currentYear}
-					<a href="https://www.kaboomshebang.com" target="_blank" rel="noopener noreferrer">
-						Kaboom Shebang
+		<>
+			<div id="menu-bg-overlay">{/* dark background overlay */}</div>
+			<div id="menu">
+				<div className="bar">
+					<img src={iconLogo.toString()} alt="Shebang" />
+					<img src={iconCross.toString()} alt="Cross" />
+				</div>
+				<div className="content-wrapper">
+					<h3>About</h3>
+					<p>The Kaboom BPM calculator: an application to calculate the tempo of a song.</p>
+					<p>Instructions: tap in the rhythm to find the tempo of a piece of music.</p>
+					<a
+						href="https://github.com/kaboomshebang/kbsb-tempo-tapper/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Source code on: Github.com
 					</a>
-				</p>
+
+					<h4>Install on iOS home-screen</h4>
+					<p>Click the on "square with arrow" icon. Scroll down. Select "Add to Home Screen".</p>
+				</div>
+				<img className="shebang" src={imgShebang.toString()} alt="The Shebang in 3D" />
+				<div className="menu-footer">
+					<p>
+						<span>Copyright &#169; {currentYear}</span>
+						<a href="https://www.kaboomshebang.com" target="_blank" rel="noopener noreferrer">
+							Kaboom Shebang
+						</a>
+					</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
