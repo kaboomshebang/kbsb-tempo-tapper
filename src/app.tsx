aa19d84957d1b486d6b916fbda7181296887af44
Fred Snyder <safe.sand1626@fastmail.com>
2022-09-23 11:33:46 +0000

    Large refactor of tempo calculation functionality

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NavBar } from './components/navBar';
 import { TapBox } from './components/tapBox';
 import { Reset } from './components/reset';
@@ -10,9 +10,13 @@ export function App() {
 	const [time, setTime] = useState([]);
 	const [taps, setTaps] = useState(0);
 	const [bpmIntervals, setBpmIntervals] = useState([]);
-	const [bpmAverages] = useState([]);
+	const [bpmAverages, setBpmAverages] = useState([]);
+	let [tempo1, setTempo1] = useState(undefined);
+	let [tempo2, setTempo2] = useState(undefined);
 
-	let tempo;
+	let tempo = 0;
+	let tempoInt = '0';
+	let tempoFloat = '0';
 
 	// calculate the bpmIntervals in beats/minute
 	function calcIntervals() {
@@ -20,7 +24,8 @@ export function App() {
 			// difference between "the last array item" - "the one before"
 			const interval = time[time.length - 1] - time[time.length - 2];
 			// 1000ms / interval === Bps * 60 === Bpm
-			bpmIntervals.push((1000 / interval) * 60);
+			const bpm = (1000 / interval) * 60;
+			bpmIntervals.push(Math.floor(bpm * 10) / 10);
 		}
 	}
 	calcIntervals();
@@ -29,25 +34,59 @@ export function App() {
 	function calcAverageBpm() {
 		if (bpmIntervals.length > 1) {
 			// sum all the bpmIntervals
-			const summedBpms = bpmIntervals.reduceRight((acc, curr, i, arr) => {
+			const summedBpms = bpmIntervals.reduceRight((acc, curr) => {
 				return acc + curr;
 			});
-			bpmAverages.push(summedBpms / bpmIntervals.length);
+			const bpm = summedBpms / bpmIntervals.length;
+			bpmAverages.push(Math.floor(bpm * 10) / 10);
 		}
 	}
 	calcAverageBpm();
 
-	function calcTempo() {
+	function calcAverageOverBPMs() {
 		if (bpmAverages.length > 1) {
 			// calculate the average of all the averages
 			const sum = bpmAverages.reduce((acc, curr) => {
 				return acc + curr;
 			});
-			return sum / bpmAverages.length;
+			return Math.floor((sum / bpmAverages.length) * 10) / 10;
 		}
 	}
 
-	tempo = calcTempo();
+	function calcAverageOverTotalTime() {
+		if (taps > 3) {
+			// for accuracy: skip the first two timestamp
+			// difference between first timestamp and latest timestamp
+			let totalDiff = time[time.length - 1] - time[2];
+			// crosstable // 60000(ms) : bpm // time(ms) : taps
+			let bps = totalDiff / (time.length - 3);
+			let bpm = (1000 / bps) * 60;
+			return Math.floor(bpm * 10) / 10;
+		}
+	}
+
+	// combine the 2 different measurements
+	function calcCombineMeasurements() {
+		tempo1 = calcAverageOverTotalTime();
+		tempo2 = calcAverageOverBPMs();
+
+		if (taps > 3) {
+			// the averageOverTotalTime weighs more
+			tempo = (tempo1 * 4 + tempo2) / 5;
+			// convert to int and float for tempo component
+			let digits = tempo.toFixed(0).toString().length;
+			tempoInt = tempo.toString().slice(0, digits);
+			tempoFloat = tempo.toFixed(1).slice(-1);
+			// slice digits for stats component
+			tempo1 = tempo1.toFixed(1);
+			tempo2 = tempo2.toFixed(1);
+		}
+	}
+	calcCombineMeasurements();
+
+	useEffect(() => {
+		console.log('Update');
+	}, [time, taps]);
 
 	return (
 		<>
