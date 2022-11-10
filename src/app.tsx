8d5c740cb7fe2f3b254650142888dd5109564c2e
Fred Snyder <safe.sand1626@fastmail.com>
2022-11-10 16:17:01 +0000

    Refactor tempo calculation

--- src/app.tsx
+++ src/app.tsx
@@ -9,79 +9,65 @@ export const App = () => {
 	// tap timestamps; number of taps; time between taps in ms
 	const [time, setTime] = useState([]);
 	const [taps, setTaps] = useState(0);
+
 	const [bpmIntervals, setBpmIntervals] = useState([]);
 	const [bpmAverages, setBpmAverages] = useState([]);
 
+	// prepare values for DOM rendering
 	let tempo = 0;
 	let tempoInt = '0';
 	let tempoFloat = '0';
 
 	// calculate the bpmIntervals in beats/minute
 	const calcIntervals = () => {
-		if (taps > 1) {
-			// difference between "the last array item" - "the one before"
-			const interval = time[time.length - 1] - time[time.length - 2];
-			// 1000ms / interval === Bps * 60 === Bpm
-			const bpm = (1000 / interval) * 60;
-			bpmIntervals.push(Math.floor(bpm * 10) / 10);
-		}
-	};
-	calcIntervals();
+		// difference between "the last array item" - "the one before"
+		const interval = time[time.length - 1] - time[time.length - 2];
+		// 1000ms / interval === Bps * 60 === Bpm
+		const bpm = (1000 / interval) * 60;
 
-	// calculate the average bpm
+		bpmIntervals.push(Math.floor(bpm * 10) / 10);
+	};
+
+	// calculate the bpm averages
 	const calcAverageBpm = () => {
+		// sum all the bpmIntervals
+		const summedBpms = bpmIntervals.reduceRight((acc, curr) => acc + curr);
+		const bpm = summedBpms / bpmIntervals.length;
+		bpmAverages.push(Math.floor(bpm * 10) / 10);
+	};
+
+	// calculate the mean of the last 10 bpm averages
+	const calcMeanLastX = (x) => {
+		// sum all the bpmIntervals
+		const last10 = bpmAverages.slice(x * -1);
+		const summedLast10 = last10.reduceRight((acc, curr) => {
+			// console.log(i, acc + curr);
+			return acc + curr;
+		});
+		const bpm = summedLast10 / last10.length;
+		// setTempo(Math.floor(bpm * 10) / 10);
+		tempo = Math.floor(bpm * 10) / 10;
+	};
+
+	// calculate the tempo
+	const executeCalculations = () => {
+		if (taps > 1) {
+			calcIntervals();
+		}
 		if (bpmIntervals.length > 1) {
-			// sum all the bpmIntervals
-			const summedBpms = bpmIntervals.reduceRight((acc, curr) => {
-				return acc + curr;
-			});
-			const bpm = summedBpms / bpmIntervals.length;
-			bpmAverages.push(Math.floor(bpm * 10) / 10);
+			calcAverageBpm();
 		}
-	};
-	calcAverageBpm();
-
-	function calcAverageOverBPMs() {
-		if (bpmAverages.length > 1) {
-			// calculate the average of all the averages
-			const sum = bpmAverages.reduce((acc, curr) => {
-				return acc + curr;
-			});
-			return Math.floor((sum / bpmAverages.length) * 10) / 10;
-		}
-	}
-
-	const calcAverageOverTotalTime = () => {
 		if (taps > 3) {
-			// for accuracy: skip the first two timestamp
-			// difference between first timestamp and latest timestamp
-			let totalDiff = time[time.length - 1] - time[2];
-			// crosstable // 60000(ms) : bpm // time(ms) : taps
-			let bps = totalDiff / (time.length - 3);
-			let bpm = (1000 / bps) * 60;
-			return Math.floor(bpm * 10) / 10;
-		}
-	};
+			taps < 10 && calcMeanLastX(2);
+			taps >= 10 && calcMeanLastX(taps - Math.floor(taps * 0.5));
 
-	// combine the 2 different measurements
-	const calcCombineMeasurements = () => {
-		tempo1 = calcAverageOverTotalTime();
-		tempo2 = calcAverageOverBPMs();
-
-		if (taps > 3) {
-			// the averageOverTotalTime weighs more
-			tempo = (tempo1 * 4 + tempo2) / 5;
-			// convert to int and float for tempo component
+			// convert tempo to int and float for tempo component
 			let digits = tempo.toFixed(0).toString().length;
 			tempoInt = tempo.toString().slice(0, digits);
 			tempoFloat = tempo.toFixed(1).slice(-1);
-			// slice digits for stats component
-			tempo1 = tempo1.toFixed(1);
-			tempo2 = tempo2.toFixed(1);
 		}
 	};
-	calcCombineMeasurements();
-
+	executeCalculations();
 
 	return (
 		<>
