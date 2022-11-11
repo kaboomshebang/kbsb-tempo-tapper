104210bafa20b80965fde97abcf57565137be0a3
Fred Snyder <safe.sand1626@fastmail.com>
2022-11-11 13:44:35 +0000

    Add import

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { NavBar } from './components/navBar';
 import { TapBox } from './components/tapBox';
 import { Reset } from './components/reset';
