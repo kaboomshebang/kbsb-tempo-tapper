b1605011f40e3d24ee3c826074b431383fc070aa
Fred Snyder <safe.sand1626@fastmail.com>
2022-11-14 13:13:24 +0000

    Add a status bar that displays the tempo accuracy

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,7 +1,13 @@
 export const Stats = (props) => {
 	return (
-		<div id="stats-wrapper" className={props.taps > 3 ? `show` : ``}>
-			<div>T: {props.taps}</div>
+		<div id="stats-wrapper" className={props.taps > 3 && props.taps < 120 && `show`}>
+			<div
+				style={{
+					left: `min(${props.taps < 90 ? props.taps : 90}%, calc(100% - 3rem) )`,
+				}}
+			>
+				T: {props.taps}
+			</div>
 		</div>
 	);
 };
